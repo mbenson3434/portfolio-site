cdc42b139797b5df164265feb2097570bf73f3f8
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2021-03-25 18:08:51 -0400

    Took link off of project logo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <a href="https://github.com/mbenson3434" target="_blank" className='hero-links'>gitHub</a>&nbsp;&nbsp;
           <a href="https://www.linkedin.com/in/meghann-benson/" target="_blank" className='hero-links'>LinkedIn</a>
         </span>
-        
+
       </section>
 
       {/* Line to divide hero section and project section */}
@@ -25,34 +25,30 @@ function App() {
         {/* Kid Bank */}
         <div className="img-div img1">
           <div className="logo-container">
-            <a className="logo" target="_blank" href="https://kid-bank.mbenson3434.vercel.app/">
-              <img className="images" src="/goat.png" alt="" />
-            </a>
+            <img className="images" src="/goat.png" alt="" />
           </div>
           <div className="caption">
-          <p>
-            <a target="_blank" href="https://kid-bank.vercel.app/">Try Kid Bank</a>
-          </p>
-          <p>
-            <a target="_blank" href="https://github.com/mbenson3434/kid-bank">gitHub</a>
-          </p>
+            <p>
+              <a target="_blank" href="https://kid-bank.vercel.app/">Try Kid Bank</a>
+            </p>
+            <p>
+              <a target="_blank" href="https://github.com/mbenson3434/kid-bank">gitHub</a>
+            </p>
           </div>
         </div>
 
         {/* Family Promise */}
         <div className="img-div img2">
           <div className="logo-container">
-            <a className="logo" target="_blank" href="https://kid-bank.mbenson3434.vercel.app/">
-              <img className="images" src="/FPlogo.png" alt="" />
-            </a>
+            <img className="images" src="/FPlogo.png" alt="" />
           </div>
           <div className="caption">
-          <p>
-            <a target="_blank" href="https://www.familypromiseofspokane.org/">FamilyPromise.org</a>
-          </p>
-          <p>
-            <a target="_blank" href="https://github.com/mbenson3434/family-promise-spokane-fe-b">gitHub</a>
-          </p>
+            <p>
+              <a target="_blank" href="https://www.familypromiseofspokane.org/">FamilyPromise.org</a>
+            </p>
+            <p>
+              <a target="_blank" href="https://github.com/mbenson3434/family-promise-spokane-fe-b">gitHub</a>
+            </p>
           </div>
         </div>
 
